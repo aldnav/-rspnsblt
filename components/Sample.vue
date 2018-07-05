facb1ca2c0fa0ee8c0369bd62d94fe2d208db0ba
Aldrin Navarro <aldrin@channelfix.com>
2018-07-05 20:22:05 +0800

    Sample server client interaction

---
+++ components/Sample.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<span><strong>{{ text }}</strong></span>
+	</div>
+</template>
+
+
+<script>
+import axios from '~/plugins/axios';
+
+export default {
+	data: () => ({
+		text: 'Anonymous'
+	}),
+
+	mounted() {
+		console.log('yo');
+		axios.get('/ohyeah').then(response => {
+			this.text = response.data;
+		});
+	}
+};
+</script>
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 """
 Usage: server.py --port=<port>
 """
+import asyncio
+import aiohttp_cors
 from aiohttp import web
 from docopt import docopt
 
@@ -8,12 +10,28 @@ from docopt import docopt
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
-    return web.Response(text=text)
+    res = {'data': text}
+    return web.json_response(res)
 
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+
+
+# app.add_routes([web.get('/', handle),
+#                 web.get('/{name}', handle)])
+app.router.add_route('GET', '/{name}', handle)
+
+# Configure default CORS settings.
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+})
+# Configure CORS on all routes.
+for route in list(app.router.routes()):
+    cors.add(route)
 
 
 if __name__ == '__main__':
